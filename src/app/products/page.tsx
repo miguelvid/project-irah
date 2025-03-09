4d9cd681f25fd302fdb3d9af2f1dd05ff120ff95
miguelvid <miguelvid.1909@gmail.com>
2025-03-09 17:55:15 -0300

    initial commit

---
+++ src/app/products/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+const ProductPage = () => {
+    return (
+        <div>
+            <h1 className="text-red-400 p-5 rounded-xl">Product</h1>
+            <Button></Button>
+        </div>
+     );
+}
+ 
+export default ProductPage;
