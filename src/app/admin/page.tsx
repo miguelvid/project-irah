3485d17c689338266e3b0334942fb8491a5cb801
miguelvid <miguelvid.1909@gmail.com>
2025-04-30 17:39:39 -0300

    build: correções para deploy

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -5,14 +5,10 @@ import { Suspense } from 'react';
 import AdminPanelContent from './AdminPanelContent';
 import { AuthProvider } from './providers/AuthProvider';
 
-function LoadingFallBack() { {
-    return (
-      <div className="flex h-screen w-full items-center justify-center">
-        <div className="h-10 w-10 animate-spin rounded-full border-4 border-solid border-primary border-t-transparent" />
-      </div>
-    );
-  }
+function LoadingFallBack() {
+    return <div>Carregando...</div>
 }
+
 export default function AdminPanel() {
   return (
     <Suspense fallback={<LoadingFallBack />}>
