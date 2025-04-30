1fea2ec918e1e33bc1e482c4f9d04f7f563b4c7f
miguelvid <miguelvid.1909@gmail.com>
2025-04-30 17:23:11 -0300

    build: correções para deploy

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,12 +1,24 @@
 'use client';
 
+import { Suspense } from 'react';
+
 import AdminPanelContent from './AdminPanelContent';
 import { AuthProvider } from './providers/AuthProvider';
 
+function LoadingFallBack() { {
+    return (
+      <div className="flex h-screen w-full items-center justify-center">
+        <div className="h-10 w-10 animate-spin rounded-full border-4 border-solid border-primary border-t-transparent" />
+      </div>
+    );
+  }
+}
 export default function AdminPanel() {
   return (
-    <AuthProvider>
-      <AdminPanelContent />
-    </AuthProvider>
+    <Suspense fallback={<LoadingFallBack />}>
+      <AuthProvider> 
+        <AdminPanelContent />
+      </AuthProvider>
+    </Suspense>
   );
 }
